6e2954813b8b8219c1cced74aee1234adadf9a91
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-09-19 03:10:44 -0600

    .

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         cargarListasSimulacino(txt)
     
         print("\n\nExito\n\n")
+        
         c.LLineas.reiniciar()
         c.LLineas.ElaborarManual("Guitarra")
         c.LLineas.ElaborarManual("Piano")
